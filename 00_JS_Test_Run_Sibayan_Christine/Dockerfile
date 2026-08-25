8d9bece54e68499b4aaaeaccbe3c95d19c58b57d
Christine Sibayan <2025-1-0389.sibayan@gmail.com>
2026-08-25 16:27:07 +0800

    00_JS_Test_Run_My_First_JavaScript_program

---
+++ 00_JS_Test_Run_Sibayan_Christine/Dockerfile
@@ -0,0 +1,4 @@
+FROM node:alpine
+WORKDIR /app
+COPY package*.json ./
+CMD ["npm", "run", "dev"]
---
+++ 00_JS_Test_Run_Sibayan_Christine/index.js
@@ -0,0 +1,3 @@
+const fullname = "Christine Sibayan";
+console.log(`Hi! ${fullname}`);
+console.log("Welcome to the JavaScript language!");
